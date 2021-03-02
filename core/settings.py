61b1acdd066133f2cdcdba814bd58d1a342c08f1
smirnovskoe <smirnovskoe@gmail.com>
2021-03-02 11:17:01 +0300

    iteration 5: create media folder
    add default.png course image

--- core/settings.py
+++ core/settings.py
@@ -115,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media config
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
