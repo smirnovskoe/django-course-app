168eff1349dfd5c6b40dd2c742d87e9d48d407fc
smirnovskoe <smirnovskoe@gmail.com>
2021-03-02 11:10:31 +0300

    iteration 3: create Course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import (
+    text,
+    timezone
+)
 
-# Create your models here.
+User = get_user_model()
+
+
+class Course(models.Model):
+    STATUS = (
+        (0, "Didn't start"),
+        (1, 'Start'),
+        (3, 'Ended'),
+    )
+
+    course_name = models.CharField(max_length=255)
+    picture = models.ImageField(upload_to='course_images/', default='course_images/default.png')
+    slug = models.SlugField(max_length=255, unique=True)
+
+    trainer = models.ForeignKey(
+        User, on_delete=models.CASCADE, )
+
+    description = models.TextField(blank=True, null=True)
+
+    date_start = models.DateField()
+    date_end = models.DateField()
+
+    publish = models.DateTimeField(default=timezone.now)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    status = models.IntegerField(
+        choices=STATUS,
+        default=0,
+    )
+
+    def save(self, *args, **kwargs):
+        self.slug = text.slugify(self.course_name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.course_name}'
